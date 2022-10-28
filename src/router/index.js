01f12a15be8fd18e80652c1de257fb90c978594d
Thanh Cong <ducong45@gmail.com>
2022-10-29 05:21:58 +0700

    Create an edit page

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,22 @@ const routes = [
         name: 'contactbook',
         component: ContactBook,
     },
-    
+
     {
         path: '/:pathMatch(.*)*',
         name: 'notfound',
         component: () => import('@/views/NotFound.vue'),
+    },
+    
+    {
+        path: '/contacts/:id',
+        name: 'contact.edit',
+        component: () => import('@/views/ContactEdit.vue'),
+        props: (route) => ({ contactId: parseInt(route.params.id) })
         },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
-    
+    routes
 });
 export default router;
